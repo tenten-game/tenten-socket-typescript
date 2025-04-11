d6cc21cf35e78812de770ad3a5da53fa365ed53b
Gwanho KIM <bigquan977@gmail.com>
2025-04-11 16:51:47 +0900

    d

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,7 +4,7 @@ const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : proc
 dotenv.config({ path: envFile });
 
 // 앱 서버명 판단 => app.tenten.games => app${number}.tenten.games
-const PROD_NUMBER: string = process.env.APP_NUMBER || '1';
+const PROD_NUMBER: string = process.env.APP_NUMBER || '';
 const ENV_SERVER_URL: string = process.env.NODE_ENV === 'image' ? 'image.tenten.games' : process.env.SERVER_URL || 'development.tenten.games'
 const SERVER_URL: string = process.env.NODE_ENV === 'production'
   ? ENV_SERVER_URL.replace('app', `app${PROD_NUMBER}`)
