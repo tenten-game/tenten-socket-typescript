4d4ee19cb13bcd8c61f98f393ad4303ad74aad39
Gwanho KIM <bigquan977@gmail.com>
2025-06-23 16:20:57 +0900

    a

--- src/application/normal/normal.ingame.service.ts
+++ src/application/normal/normal.ingame.service.ts
@@ -1,24 +1,24 @@
 import { User } from "../../common/entity/user.entity";
-import { addUserToGameQueue, addUserFloorResult, getFirstUserInQueue, getGameResultsByFloor, clearGameQueue, clearGameResults } from "../../repository/normal/ingame.repository";
+import { add6030Do, add6040Do, get6030Do, get6040Finish, remove6030, remove6040 } from "../../repository/normal/ingame.repository";
 import { NormalInGame6040DoRequest } from "./dto/request";
 
 export async function handleNormalInGame6030Do(roomNumber: string, userId: number): Promise<number> {
-    await addUserToGameQueue(roomNumber, userId);
-    return await getFirstUserInQueue(roomNumber);
+    await add6030Do(roomNumber, userId);
+    return await get6030Do(roomNumber);
 }
 
 export async function handleNormalInGame6030DoAfter(roomNumber: string): Promise<void> {
-    await clearGameQueue(roomNumber);
+    await remove6030(roomNumber);
 }
 
 export async function handleNormalInGame6040Do(roomNumber: string, user: User, request: NormalInGame6040DoRequest): Promise<void> {
-    await addUserFloorResult(roomNumber, user, request.floorData);
+    await add6040Do(roomNumber, user, request.floorData);
 }
 
 export async function handleNormalInGame6040Finish(roomNumber: string): Promise<Record<number, User[]>> {
-    return await getGameResultsByFloor(roomNumber);
+    return await get6040Finish(roomNumber);
 }
 
 export async function handleNormalInGame6040FinishAfter(roomNumber: string): Promise<void> {
-    await clearGameResults(roomNumber);
+    await remove6040(roomNumber);
 }
